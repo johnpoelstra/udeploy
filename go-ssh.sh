b7407c919ae3352ba5467bcc14bbb938428a6c29
jp <poelcat@gmail.com>
2022-05-19 14:40:10 -0700

    more tweaks on deployment

--- go-ssh.sh
+++ go-ssh.sh
@@ -1,6 +1,7 @@
 # https://linuxize.com/post/how-to-enable-ssh-on-ubuntu-20-04/
 sudo apt update
-sudo apt install -y openssh-server
+sudo apt install -y openssh-server screen htop vim
 sudo ufw allow ssh
 ip a | grep 192.168
+ssh-import-id-gh johnpoelstra
 
